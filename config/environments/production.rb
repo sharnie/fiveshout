3c1c6919963c9d631d338f3e5f6d1e9c9f89e1ab
Sharnie Ivery <sharnieivery@gmail.com>
2014-07-20 11:12:29 -0400

    Add production mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,15 @@ Rails.application.configure do
 
 
   ## Mailer Options
-  # config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address => "127.0.0.1",
-  #   :port    => 25,
-  #   :domain  => 'yourdomain.com'
-  # }
+  config.action_mailer.default_url_options = { host: 'http://fiveshout.herokuapp.com/' }
+  config.action_mailer.delivery_method     = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            "charmainebrown6@gmail.com",
+    password:             "Somethingweird1",
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 end
